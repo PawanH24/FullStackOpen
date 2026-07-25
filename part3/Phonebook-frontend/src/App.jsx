a7120d74eb2e00d1331ef56da018a745df51c6c6
Pawan Hada <hadapawan13@gmail.com>
2026-07-25 13:58:38 +0545

    ex 3.11 completed

---
+++ part3/Phonebook-frontend/src/App.jsx
@@ -0,0 +1,163 @@
+import { useState, useEffect } from "react";
+
+import personService from "./services/person";
+import Notification from "./components/Notification";
+
+const List = ({ persons, onDelete }) => {
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={person.id}>
+          {person.name} {person.number}{" "}
+          <button onClick={() => onDelete(person.id)}>delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Filter = ({ value, onChange }) => {
+  return (
+    <div>
+      filter shown with <input value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+const PersonForm = ({
+  onSubmit,
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [successMsg, setSuccessMsg] = useState(null);
+
+  useEffect(() => {
+    personService.getAll().then((initialPerson) => setPersons(initialPerson));
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/persons")
+  //     .then((response) => response.json())
+  //     .then((data) => setPersons(data));
+  // }, []);
+  function handleDelete(id) {
+    const personToDelete = persons.find((person) => id === person.id);
+    const del = confirm(`Delete ${personToDelete.name} ?`);
+    if (del)
+      personService.deletePerson(id).then(() => {
+        setPersons(persons.filter((c) => c.id !== id));
+      });
+  }
+
+  function checkDuplicate() {
+    for (const person of persons) {
+      if (person.name === newName) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function handleNameChange(event) {
+    setNewName(event.target.value);
+  }
+
+  function handleNumberChange(event) {
+    setNewNumber(event.target.value);
+  }
+  function handleFilterChange(e) {
+    setFilter(e.target.value);
+  }
+
+  function addName(event) {
+    event.preventDefault();
+
+    if (checkDuplicate()) {
+      const newNum = confirm(
+        `${newName} is already added to phonebook,replace the old number with a new one?`,
+      );
+      const obj = persons.find((person) => newName === person.name);
+      if (newNum) {
+        personService
+          .update(obj.id, { ...obj, number: newNumber })
+          .then((response) => {
+            const updatedPersons = persons.map((c) =>
+              c.id === response.id ? { ...c, number: response.number } : c,
+            );
+            //console.log(updatedPersons);
+            setPersons(updatedPersons);
+          });
+      }
+    } else {
+      const newPerson = {
+        name: newName,
+        number: newNumber,
+      };
+
+      // setPersons(persons.concat(newPerson));
+      // axios
+      //   .post("http://localhost:3001/persons", newPerson)
+      //   .then((response) => {
+      //     setPersons(persons.concat(response.data));
+      //   });
+      personService.create(newPerson).then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        setSuccessMsg(`Added ${returnedPerson.name}`);
+        setTimeout(() => {
+          setSuccessMsg(null);
+        }, 2000);
+      });
+    }
+    setNewName("");
+    setNewNumber("");
+  }
+
+  const personToShow =
+    filter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase()),
+        );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={successMsg} />
+      <Filter value={filter} onChange={handleFilterChange} />
+      <h2>Add a new</h2>
+      <PersonForm
+        onSubmit={addName}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <List persons={personToShow} onDelete={handleDelete} />
+    </div>
+  );
+};
+
+export default App;
