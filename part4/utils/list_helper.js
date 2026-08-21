e086e47cc67df085d6ff02e5ab205cf179b3e4b5
Pawan Hada <hadapawan13@gmail.com>
2026-08-21 17:04:23 +0545

    Completed till 4.5 ex

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes =(blogs) => {
+  return blogs.reduce((totalLikes,blog) => totalLikes+blog.likes,0)
+}
+
+const favoriteBlog =(blogs) => {
+  let highest = {}
+  let c =0
+  blogs.map(blog => {
+    if(blog.likes>c){
+      c=blog.likes
+      highest = blog}
+  })
+  return highest
+}
+
 module.exports = {
-  dummy
+  dummy,totalLikes,favoriteBlog
 }
